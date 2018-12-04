427022056506d7008dd9e333b4530aaba0ee55a7
linjunpeng <linjunpeng@inke.cn>
2018-12-04 14:26:14 +0800

    bug fix

--- study_python/sort.py
+++ study_python/sort.py
@@ -13,8 +13,10 @@ import redis
 from utils.redis import RedisConfig
 
 import sys
-#reload(sys)
-#sys.setdefaultencoding('utf-8')
+
+
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
 def inti_shop_tab():
     r = redis.Redis(host='127.0.0.1', port=6379, db=0)
     # r.set('hello','world')
@@ -27,12 +29,12 @@ def inti_shop_tab():
     tab_list.append(dict3)
     tab_list = sorted(tab_list, key=lambda x: x['sort'])
     print(tab_list)
-    r.hmset('ckey',dict1)
+    r.hmset('ckey', dict1)
     print(r.hgetall('ckey'))
     # import redis
     # r = redis.Redis(host='127.0.0.1',port=6379,db=0)
     # r.set('hello','world')
-    #print(redis.get('hello'))
+    # print(redis.get('hello'))
 
 
 def lottery_update_white(uid, status):
@@ -53,6 +55,7 @@ def lottery_update_white(uid, status):
             list.remove(uid)
     r.set(white_key, list)
 
+
 def hget_test():
     r = redis.Redis(host='127.0.0.1', port=6379, db=0)
 
@@ -66,46 +69,53 @@ def hget_test():
     # print(len(data))
     period_record = {}
     period_record['activity_name'] = '中国'
-    r.set("aa",'重中之重')
+    r.set("aa", '重中之重')
     print(r.get('aa'))
+
 
 def test():
     user_count = 0
-    user_list = [54036,54036]
+    user_list = [54036, 54036]
     uid = 54036
-    user_count_list =[]
+    user_count_list = []
     for u in user_list:
         if u:
             if int(u) == int(uid):
                 user_count_list.append(u)
     print(len(user_count_list))
 
+
 def is_None(data):
-    if  data!=0 or data == 'None' or data is None:
+    if data != 0 or data == 'None' or data is None:
         return True
     else:
         return False
+
 
 def redis_expire():
     r = redis.Redis(host='127.0.0.1', port=6379, db=0)
     ret = r.set("aa", 1)
     print(ret)
     r.expire('aa', 10)
+
+
 def get_data(aa):
     r = redis.Redis(host='127.0.0.1', port=6379, db=0)
     print(r.get(aa))
-    list = ['a','b']
-    r.hset('a','b',list)
-    r.hset('a','c',1)
+    list = ['a', 'b']
+    r.hset('a', 'b', list)
+    r.hset('a', 'c', 1)
     k = r.hgetall('a')
     print(k)
 
+
 def get_rez():
     r = redis.Redis(host='127.0.0.1', port=6379, db=0)
-    r.zadd("page_size",10,"baidu")
+    r.zadd("page_size", 10, 1.1)
     print(r.zcard("a"))
+
 
 if __name__ == '__main__':
     # lottery_update_white(1111, 1)
-    #redis_expire()
+    # redis_expire()
     get_rez()
--- study_python/split.py
+++ study_python/split.py
@@ -20,6 +20,12 @@ def rand():
     print(type(rand))
     print(rand)
 
+def vaci():
+    str = "41062119910310151X"
+    data = str.split('')[6,10]
+    print(data)
+
+
 def yu():
     # count = 0
     # max_count = 5
@@ -34,30 +40,49 @@ def yu():
     # print(str)
 
 
-    random_round =[{u'rate': u'10%', u'id': 1, u'round': 1}, {u'rate': u'20%', u'id': 2, u'round': 2}, {u'rate': u'50%', u'id': 3, u'round': 3}, {u'rate': u'80%', u'id': 4, u'round': 4}, {u'rate': u'100%', u'id': 5, u'round': 5}],random_item=[{u'item_id': 1001, u'item_name': u'\u91d1\u5e01', u'rate': u'20%', u'id': 1, u'item_count': 100}, {u'item_id': 1005, u'item_name': u'\u5e78\u8fd0\u7b26', u'rate': u'20%', u'id': 2, u'item_count': 10}, {u'item_id': 1002, u'item_name': u'\u829d\u58eb\u8c46', u'rate': u'50%', u'id': 3, u'item_count': 10}, {u'item_id': 1501, u'item_name': u'1\u5143\u5fae\u4fe1\u7ea2\u5305', u'rate': u'10%', u'id': 4, u'item_count': 1}]
+    #random_round =[{u'rate': u'10%', u'id': 1, u'round': 1}, {u'rate': u'20%', u'id': 2, u'round': 2}, {u'rate': u'50%', u'id': 3, u'round': 3}, {u'rate': u'80%', u'id': 4, u'round': 4}, {u'rate': u'100%', u'id': 5, u'round': 5}],random_item=[{u'item_id': 1001, u'item_name': u'\u91d1\u5e01', u'rate': u'20%', u'id': 1, u'item_count': 100}, {u'item_id': 1005, u'item_name': u'\u5e78\u8fd0\u7b26', u'rate': u'20%', u'id': 2, u'item_count': 10}, {u'item_id': 1002, u'item_name': u'\u829d\u58eb\u8c46', u'rate': u'50%', u'id': 3, u'item_count': 10}, {u'item_id': 1501, u'item_name': u'1\u5143\u5fae\u4fe1\u7ea2\u5305', u'rate': u'10%', u'id': 4, u'item_count': 1}]
     # for i in range(len(list)):
     #     b = list[i]
     #     print(b['a'])
-    play_time = 0
-    round = None
-    for i in range(len(random_round)-1):
-        print(i)
-        round_one = random_round[i]['a']
-        round_two = random_round[i + 1]['a']
-        if round_one>=play_time:
-            round = random_round[i]
-            break
-
-        if round_one <= play_time and play_time < round_two:
-            round = random_round[i]
-            break
-    print(len(random_round))
-    if round is None:
-        round = random_round[len(random_round)-1]
-    print(round)
+    # play_time = 0
+    # round = None
+    # for i in range(len(random_round)-1):
+    #     print(i)
+    #     round_one = random_round[i]['a']
+    #     round_two = random_round[i + 1]['a']
+    #     if round_one>=play_time:
+    #         round = random_round[i]
+    #         break
+    #
+    #     if round_one <= play_time and play_time < round_two:
+    #         round = random_round[i]
+    #         break
+    # print(len(random_round))
+    # if round is None:
+    #     round = random_round[len(random_round)-1]
+    # print(round)
 
     for i in range(2):
         print(i)
 
+def vaci():
+    str = "41062119910310151X"
+    data = str[6:10]
+    print(data)
+    start_time = (datetime.datetime.now()).strftime("%Y")
+    print(start_time)
+    if int(start_time)-int(data)>18:
+        print("11111111")
+def rand():
+    ret = random.uniform(0, 1)
+    print(type(ret))
+    print(ret)
+def fot_index():
+    fruits = ['banana', 'apple', 'mango']
+    for index in range(len(fruits)):
+        print(fruits[index])
+        print(index)
+        print(len(fruits))
+
 if __name__ == '__main__':
-    yu()
+    fot_index()
